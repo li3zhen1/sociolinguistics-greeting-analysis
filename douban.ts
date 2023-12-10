89ab3ab9ac192c06bdb8af34976ec9c07297638d
Li, Zhen <zhenli.craig@gatech.edu>
2023-12-10 14:21:39 -0500

    clean 1960

--- douban.ts
+++
@@ -1,8 +0,0 @@
-import DoubanData from "./douban.json"
-// import fs
-import fs from "fs"
-
-const titles = DoubanData.map(it => it.children.props.data.title)
-
-// save to json
-fs.writeFileSync("./douban_titles.json", JSON.stringify(titles))
